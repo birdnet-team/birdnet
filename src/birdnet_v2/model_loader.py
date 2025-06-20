462eba2bc14224ac4dbfa17851a3c9c17b2e6212
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-06-20 08:52:01 +0200

    fix

--- src/birdnet_v2/model_loader.py
+++ src/birdnet_v2/model_loader.py
@@ -112,7 +112,7 @@ if __name__ == "__main__":
   start = time.perf_counter()
   result = model.analyze(
     audio_paths,
-    n_jobs=24,
+    n_jobs=1,
     batch_size=1,
     n_slots_factor=2,
     apply_sigmoid=False,
