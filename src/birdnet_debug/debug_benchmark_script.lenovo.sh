548df3e7d15eca7879d133d21f0dce04632ed578
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-07-08 12:10:55 +0200

    .

--- src/birdnet_debug/debug_benchmark_script.lenovo.sh
+++ src/birdnet_debug/debug_benchmark_script.lenovo.sh
@@ -2,3 +2,7 @@
 birdnet-benchmark example/soundscape.wav /tmp/result.csv --serial-io -w 1
 
 birdnet-benchmark src\\birdnet_v2_debug /tmp/result.csv -w 4 -f 1
+#  61 x real-time (RTF: 0.01631669)
+
+birdnet-benchmark src\\birdnet_v2_debug\\60min.wav /tmp/result.csv -w 8 -f 1
+#  67 x real-time (RTF: 0.01501786)
