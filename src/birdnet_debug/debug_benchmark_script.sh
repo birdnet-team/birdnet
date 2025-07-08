8578cce6d5745c9c490e293c7993faefa0b856fb
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-07-08 08:34:42 +0200

    .

--- src/birdnet_debug/debug_benchmark_script.sh
+++ src/birdnet_debug/debug_benchmark_script.sh
@@ -8,10 +8,29 @@ cd ~
 source .venv-bn/bin/activate
 pip install birdnet-0.1.7-py3-none-any.whl --force-reinstall
 
-birdnet-benchmark example/soundscape.wav /tmp/soundscape.csv
-birdnet-benchmark test-dataset/POW /tmp/soundscape.csv -w 2 -d CPU CPU
+# Birdnet benchmark command line tool
 
-birdnet-benchmark test-dataset/POW /tmp/soundscape.csv -k 6522 -c -100
+## Install 
 
-birdnet-benchmark test-dataset/POW /tmp/soundscape.csv -b "pb"
+## with GPU support
+pip install birdnet-0.2.0a0-py3-none-any.whl[and-cuda]
 
+## without GPU support
+pip install birdnet-0.2.0a0-py3-none-any.whl
+
+# Example usage of the 
+
+## Predict top 5 species for each segment using CPU und TFLite backend
+birdnet-benchmark example/soundscape.wav /tmp/result.csv
+
+## Predict all species
+birdnet-benchmark test-dataset/POW /tmp/result.csv --top-k 6522 --confidence -100
+
+## Use protobuf backend
+birdnet-benchmark test-dataset/POW /tmp/result.csv -b "pb"
+
+## Run on single GPU
+birdnet-benchmark test-dataset/test_dataset_4x60min /tmp/result.csv --backend "pb" --worker 1 --device "GPU" --batch-size 1000
+
+## Run on multiple GPUs
+birdnet-benchmark test-dataset/test_dataset_4x60min /tmp/result.csv --backend "pb" --worker 3 --device "GPU:0" "GPU:1" "GPU:2" --batch-size 1000
