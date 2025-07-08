05a18faee555a196dd8d63e9625dcd9b2adee5bf
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-07-08 15:02:03 +0200

    .

--- src/birdnet_debug/debug_benchmark_script.lenovo.sh
+++ src/birdnet_debug/debug_benchmark_script.lenovo.sh
@@ -6,3 +6,14 @@ birdnet-benchmark src\\birdnet_v2_debug /tmp/result.csv -w 4 -f 1
 
 birdnet-benchmark src\\birdnet_v2_debug\\60min.wav /tmp/result.csv -w 8 -f 1
 #  67 x real-time (RTF: 0.01501786)
+
+
+pip install build
+python.exe -m build -o dist/
+
+# CMD
+"C:\Program Files\Python311\python.exe" -m venv .venv-bn
+.venv-bn\\Scripts\\activate
+python.exe -m pip install --upgrade pip
+python.exe -m pip install wheel
+python.exe -m pip install ..\\dist\\birdnet-0.2.0a0-py3-none-any.whl
