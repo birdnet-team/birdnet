fd423ad79a9579f1b772ae6c77a4149148fb40c5
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-07-08 08:41:14 +0200

    .

--- src/birdnet_debug/debug_benchmark_script.sh
+++ src/birdnet_debug/debug_benchmark_script.sh
@@ -1,6 +1,7 @@
 birdnet-benchmark example/soundscape.wav /tmp/soundscape.csv
 
 birdnet-benchmark.exe example/soundscape.wav soundscape.csv
+birdnet-benchmark src\\birdnet_v2_debug /tmp/soundscape2.csv -w 1
 
 # raspberry
 
