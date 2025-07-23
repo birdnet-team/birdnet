7ff9025141d4767599ab18dd529df1d86ec0b49a
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-07-23 16:15:27 +0200

    pipeline

--- src/birdnet/acoustic_models/inference/emb/benchmarking.py
+++ src/birdnet/acoustic_models/inference/emb/benchmarking.py
@@ -1,31 +1,12 @@
 from __future__ import annotations
 
-import inspect
-import multiprocessing as mp
-import platform
-from collections import OrderedDict
-from dataclasses import asdict, dataclass
-from datetime import datetime, timedelta
-from typing import Any
+from dataclasses import dataclass
 
-import numpy as np
-import psutil
 
 from birdnet.acoustic_models.inference.benchmarking import (
   FullBenchmarkMetaBase,
   MinimalBenchmarkMetaBase,
 )
-from birdnet.backends import (
-  litert_installed,
-  tf_installed,
-)
-from birdnet.globals import (
-  ACOUSTIC_MODEL_VERSIONS,
-  MODEL_BACKENDS,
-  MODEL_PRECISIONS,
-  MODEL_TYPES,
-)
-from birdnet.local_data import get_package_version
 
 
 @dataclass
--- src/birdnet/acoustic_models/v2_4/banchmarking.py
+++ src/birdnet/acoustic_models/v2_4/banchmarking.py
@@ -1,16 +1,7 @@
-from birdnet.acoustic_models.inference.scores.benchmarking import (
-  FullBenchmarkMeta,
-  MinimalBenchmarkMeta,
-)
 from birdnet.acoustic_models.inference.emb.benchmarking import (
   FullBenchmarkEmbMeta,
   MinimalBenchmarkEmbMeta,
 )
-
-MinimalBenchmarkMetaV2_4 = MinimalBenchmarkMeta
-
-FullBenchmarkMetaV2_4 = FullBenchmarkMeta
-
 
 MinimalBenchmarkEmbMetaV2_4 = MinimalBenchmarkEmbMeta
 
