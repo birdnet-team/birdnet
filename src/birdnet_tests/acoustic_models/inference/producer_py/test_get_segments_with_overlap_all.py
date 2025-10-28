b235fff89e787f60161945a8659b47e597c802e2
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-10-28 16:19:20 +0100

    fix test

--- src/birdnet_tests/acoustic_models/inference/producer_py/test_get_segments_with_overlap_all.py
+++ src/birdnet_tests/acoustic_models/inference/producer_py/test_get_segments_with_overlap_all.py
@@ -1,3 +1,5 @@
+import numpy.testing
+
 from birdnet.acoustic_models.inference.producer import get_segments_with_overlap_all
 
 
@@ -19,14 +21,14 @@ def test_2_2_1__returns_02() -> None:
   result = list(get_segments_with_overlap_all(2, 2, 1))
   assert result == [
     (0, 2),
+    (1, 2),  # last segment shorter because it reaches the end
   ]
 
 
 def test_2_2_1p9__returns_02() -> None:
   result = list(get_segments_with_overlap_all(2, 2, 1.9))
-  assert result == [
-    (0, 2),
-  ]
+  assert_result = [(0.1 * i, 2) for i in range(20)]
+  numpy.testing.assert_allclose(result, assert_result, rtol=1e-8)
 
 
 def test_4_2_0__returns_02_24() -> None:
@@ -37,8 +39,18 @@ def test_4_2_0__returns_02_24() -> None:
   ]
 
 
-def test_4_2_0__returns_float() -> None:
+def test_int_input__returns_float() -> None:
   result = list(get_segments_with_overlap_all(4, 2, 0))
+
+  assert len(result) == 2
+  assert isinstance(result[0][0], float)
+  assert isinstance(result[0][1], float)
+  assert isinstance(result[1][0], float)
+  assert isinstance(result[1][1], float)
+
+
+def test_float_input__returns_float() -> None:
+  result = list(get_segments_with_overlap_all(4.0, 2.0, 0.0))
 
   assert len(result) == 2
   assert isinstance(result[0][0], float)
@@ -64,6 +76,7 @@ def test_6_2_1__returns_02_13_24_35_46() -> None:
     (2, 4),
     (3, 5),
     (4, 6),
+    (5, 6),  # last segment shorter because it reaches the end
   ]
 
 
