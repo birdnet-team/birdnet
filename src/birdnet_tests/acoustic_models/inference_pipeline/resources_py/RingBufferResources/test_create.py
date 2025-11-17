92e565da90c5ec8ad1cc8f8cb631569c51870199
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-11-17 09:37:48 +0100

    add comment

--- src/birdnet_tests/acoustic_models/inference_pipeline/resources_py/RingBufferResources/test_create.py
+++ src/birdnet_tests/acoustic_models/inference_pipeline/resources_py/RingBufferResources/test_create.py
@@ -10,10 +10,11 @@ def test_ring_names_have_max_30_chars() -> None:
   result = RingBufferResources._create(
     get_session_id(), 4, 1, 48_000 * 3, np.dtype(np.uint32), 1000
   )
-  allowed_max_length = 30
   # on macOS the limit is 31 including "/" at the start
   # e.g., this was too long:
   # /bn_ring_file_indices_53879_8340719616_1763049803980550000
+  # /bn_ring_file_indices_53879_834 <- this is maximum
+  allowed_max_length = 30
 
   assert len(result.rf_audio_samples.name) <= allowed_max_length
   assert len(result.rf_segment_indices.name) <= allowed_max_length
