c51dd80ab23bc6c68cc4ee725c4ab26380f07ff4
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2025-11-26 17:57:27 +0100

    rm code

--- src/birdnet_tests/test_conv.py
+++ src/birdnet_tests/test_conv.py
@@ -1,10 +1,4 @@
-import tempfile
-from hashlib import sha1
-from pathlib import Path
 
-from birdnet.acoustic_models.inference.scores.prediction_result import (
-  PredictionResultBase,
-)
 from birdnet.model_loader import load
 from birdnet_tests.test_files import TEST_FILE_LONG
 
